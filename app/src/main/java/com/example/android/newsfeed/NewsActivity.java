4ce1f8dd071a5462eafcf71355782aa49bbdd08a
Kopacz Levente <office@imageservices.ro>
2018-05-31 13:57:25 +0300

    NewsActivity modified

--- app/src/main/java/com/example/android/newsfeed/NewsActivity.java
+++ app/src/main/java/com/example/android/newsfeed/NewsActivity.java
@@ -1,13 +1,112 @@
 package com.example.android.newsfeed;
 
+import android.app.LoaderManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NewsActivity extends AppCompatActivity {
 
+    public static final String LOG_TAG = NewsActivity.class.getName();
+    private static final int NEWS_LOADER_ID = 1;
+
+    /** JSON url String */
+    private static final String JSON_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=5&limit=10";
+
+    /** Adapter for the list of earthquakes */
+    private NewsAdapter mAdapter;
+    TextView emptyState;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        // Find a reference to the {@link ListView} in the layout
+        ListView newsListView = (ListView) findViewById(R.id.list);
+        // Find a reference to the {@link ListView} in the layout
+        emptyState = (TextView)findViewById(R.id.empty_state);
+        newsListView.setEmptyView(emptyState);
+        // Find a reference to the {@link ListView} in the layout
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+
+        // Create a new {@link ArrayAdapter} of earthquakes
+        mAdapter = new NewsAdapter(this, new ArrayList<News>());
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        newsListView.setAdapter(mAdapter);
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo == null || !networkInfo.isConnected() ||
+                (networkInfo.getType() != ConnectivityManager.TYPE_WIFI
+                        && networkInfo.getType() != ConnectivityManager.TYPE_MOBILE)) {
+            emptyState.setText("No network connection");
+            progressBar.setVisibility(View.GONE);
+        } else {
+            LoaderManager loaderManager = getLoaderManager();
+            loaderManager.initLoader(NEWS_LOADER_ID, null, this);
+        }
+
+        // Set an item click listener on the ListView, which sends an intent to a web browser
+        // to open a website with more information about the selected earthquake.
+        newsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // Find the current earthquake that was clicked on
+                News currentEarthquake = mAdapter.getItem(position);
+
+                // Convert the String URL into a URI object (to pass into the Intent constructor)
+                Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
+
+                // Create a new intent to view the earthquake URI
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
+
+                // Send the intent to launch a new activity
+                startActivity(websiteIntent);
+            }
+        });
+    }
+
+
+    @Override
+    public Loader<List<News>> onCreateLoader(int id, Bundle args) {
+        Log.e(LOG_TAG,"onCreateLoader");
+        return new EarthquakeLoader(this, JSON_URL);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<News>> loader, List<News> data) {
+        // Clear the adapter of previous earthquake data
+        mAdapter.clear();
+        Log.e(LOG_TAG,"onLoadFinished");
+        // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
+        // data set. This will trigger the ListView to update.
+        if (data != null && !data.isEmpty()) {
+            mAdapter.addAll(data);
+        } else {
+            emptyState.setText("No records found");
+        }
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<News>> loader) {
+        Log.e(LOG_TAG,"onLoaderReset");
+        mAdapter.clear();
     }
 }
